6b77166651a56b90111f7a732e4c59e07004fe1b
92ada <11612325@mail.sustc.edu.cn>
2019-01-14 14:02:17 +0800

    [fix]bugs

--- assignment2/multiAgents.py
+++ assignment2/multiAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -74,17 +74,47 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
+
+        # the part of effect of food distance
         position = list(newPos)
-        food_position = newFood.asList()
-        food_list = [manhattanDistance(position, x) for x in food_position]
-        inverse_pellet_dist = 1.0 / min(food_list) if len(food_list) > 0 and min(food_list) != 0 else 0
-        ghost_list = [manhattanDistance(position, x.getPosition) for x in newGhostStates]
-        nearest_ghost_dist = min(ghost_list)
-        t_buff = 0
-        time_score = sum([time for time in newScaredTimes])
-        if time_score:
-            t_buff = -1 * nearest_ghost_dist + 20
-        return successorGameState.getScore() + time_score + (nearest_ghost_dist + t_buff) * inverse_pellet_dist + (0 if nearest_ghost_dist > 1 else -100000)
+        foodPos = newFood.asList()
+        foodList = []
+        for food in foodPos:
+          foodList.append(manhattanDistance(position,food))
+          # calculate the each distance of food to new position
+        #nearestPelletDist = min(foodList)
+        inversePelletDist = 0
+        if len(foodList)>0 and min(foodList) != 0:
+          inversePelletDist = 1.0/min(foodList)
+
+        #the part of effect of ghost distance and it is positive related to score
+        #so it do not need to inverse
+        ghostList = []
+        for ghost in newGhostStates:
+          ghostPos = ghost.getPosition()
+          ghostList.append(manhattanDistance(position,ghostPos))
+        nearestGhostDist = min(ghostList)
+
+        #the part of effect of scared time and it is also positive related and no inverse
+        #time buff is used to let pacman go to the food directly when ghosts can not eat it
+        timebuff = 0
+        timescore = 0
+        for time in newScaredTimes:
+          timescore = time + timescore
+        if timescore !=0:
+          timebuff = -1 * nearestGhostDist + 20
+
+        # prevent pacman from die
+        buff = 0
+        if nearestGhostDist <=1:
+          buff = -100000
+        #finalSocre = successorGameState.getScore()+ 0 + (timebuff+nearestGhostDist) + 100*inversePelletDist
+        finalScore = successorGameState.getScore() + timescore + (nearestGhostDist+timebuff)*inversePelletDist + buff
+        return finalScore
+
+        #print newGhostStates[1].getPosition()
+        #print newFood.asList()[0]
+        #return successorGameState.getScore()
 
 
 def scoreEvaluationFunction(currentGameState):
@@ -140,33 +170,41 @@ class MinimaxAgent(MultiAgentSearchAgent):
             Returns the total number of agents in the game
         """
         "*** YOUR CODE HERE ***"
-        def max_value(state, depth=1):
-            if state.isWin() or state.isLose():
-                return self.evaluationFunction(state)
-            result = []
-            for direction in state.getLegalActions(0):
-                result.append(min_value(state.generateSuccessor(0, direction), depth))
-            return max(result)
+        # function max_value is ued to find the max value from the minimums
+        # of ghost step
+        def max_value(state,depth):
+          depth = depth +1
+          if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+          if depth == self.depth:
+            return self.evaluationFunction(state)
 
-        def min_value(state, depth, ghostNumber=1):
-            if state.isWin() or state.isLose() or depth == self.depth:
-                return self.evaluationFunction(state)
-            values = []
-            for direction in state.getLegalActions(ghostNumber):
-                if ghostNumber < state.getNumAgents() - 1:
-                    values.append(min_value(state.generateSuccessor(ghostNumber, direction), depth, ghostNumber + 1))
-                if ghostNumber == state.getNumAgents() - 1:
-                    values.append(max_value(state.generateSuccessor((state.getNumAgents() - 1), direction), depth + 1))
-            return min(values)
-        legal_moves = gameState.getLegalActions(1)
-        scores = [max_value(gameState.generateSuccessor(1, action)) for action in legal_moves]
-        best_score = min(scores)
-        index = 0
-        for i in range(len(scores)):
-            if scores[i] == best_score:
-                index = i
-        return legal_moves[index]
-        
+          v = float('-Inf')
+          for direction in state.getLegalActions(0):
+            v = max(v,min_value(state.generateSuccessor(0,direction),depth))
+          return v
+
+        # fuction min_value generate the score of ghost step because the number of
+        # ghosts may be more than one, so this step may contain mutiple layers of tree
+        # ghostNumber means the index of the begining ghost so default is 1 and default depth is 0
+        def min_value(state,depth=0,ghostNumber=1):
+          if state.isWin() or state.isLose():
+           return self.evaluationFunction(state)
+          v = float('Inf')
+          for direction in state.getLegalActions(ghostNumber):
+            if ghostNumber < state.getNumAgents()-1:
+              v = min(v,min_value(state.generateSuccessor(ghostNumber,direction),depth,ghostNumber+1))
+            if ghostNumber == state.getNumAgents()-1:
+              v = min(v,max_value(state.generateSuccessor((state.getNumAgents()-1),direction),depth))
+          return v
+
+        legalMoves = gameState.getLegalActions(0)
+        scores = [min_value(gameState.generateSuccessor(0,action)) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)
+        return legalMoves[chosenIndex]
+
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
@@ -176,15 +214,15 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
     def expectimax_value (self, gameState, agentIndex, nodeDepth):
         if(agentIndex >= gameState.getNumAgents()):
             agentIndex = 0
-            nodeDepth += 1	
+            nodeDepth += 1
         if( nodeDepth == self.depth ):
             return self.evaluationFunction(gameState)
         if( agentIndex == self.index ):
-            return self.max_value(gameState, agentIndex, nodeDepth)        
-      
+            return self.max_value(gameState, agentIndex, nodeDepth)
+
         else:
             return self.exp_value(gameState, agentIndex, nodeDepth)
-        
+
         return 'None'
 
     def max_value (self, gameState, agentIndex, nodeDepth):
@@ -213,7 +251,7 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         return self.evaluationFunction(gameState)
       value = 0
       pr = 1.0/len(gameState.getLegalActions(agentIndex))
-      
+
       for legalActions in gameState.getLegalActions(agentIndex):
         if( legalActions == Directions.STOP):
           continue
@@ -228,55 +266,69 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
           All ghosts should be modeled as choosing uniformly at random from their
           legal moves.
         """
-        return self.expectimax_value(gameState,0,0)    
-    
+        return self.expectimax_value(gameState,0,0)
+
 
 def betterEvaluationFunction(currentGameState):
     """
       Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
-      evaluation function (question 3). 
+      evaluation function (question 3).
 
-      DESCRIPTION: In this part, I consider these factor to evaluate the performence. And add some factor to realize 
+      DESCRIPTION: In this part, I consider these factor to evaluate the performence. And add some factor to realize
       ghost-hunting, pellet-nabbing, food-gobbling, unstoppable.
       1,nearest Pellet Distance: make pacman to eat the near food which is like the power to drive pacman to go. and it
       negative related to the score so we use the neagtive value.
-      2,nearest Ghost Distance: warn pacman to keep ghost away. when the distance is less than one which means it is very 
+      2,nearest Ghost Distance: warn pacman to keep ghost away. when the distance is less than one which means it is very
       dangerous,a lifebuff ,a very small value,will work to decrease the score dramatically and minmax tree will throw this action.
       3,foodstuff : use to appeal pacman to go to the area where lots of food located in.
       4,big pellet: timescore is a factor which affect the pacman to eat big pellet, because timescore increase the score.
-      5,Scared time: when pacman eat big pellet, game go into scared time and pacman do not scare ghost, so a timebuff will 
+      5,Scared time: when pacman eat big pellet, game go into scared time and pacman do not scare ghost, so a timebuff will
       offset the lifebuff and pacman will take food as the first place and increase the finnal gamescore.
     """
     "*** YOUR CODE HERE ***"
     score = currentGameState.getScore()
     pacman_pos = currentGameState.getPacmanPosition()
-    food_pos = currentGameState.getFood().asList()
-    ghosts = currentGameState.getGhostStates()      
-    
-    food_list = []
-    for food in food_pos:
-        food_list.append(manhattanDistance(pacman_pos, food))
-    nearest_pellet_dist = 0
-    if len(food_list) > 0:
-        nearest_pellet_dist = min(food_list)
-    ghost_list = []
+    foodPos = currentGameState.getFood().asList()
+    ghosts = currentGameState.getGhostStates()
+
+    # the part of effect of the nearest food distance
+    foodList = []
+    for food in foodPos:
+      foodList.append(manhattanDistance(pacman_pos,food))
+      # calculate the each distance of food to new position
+    nearestPelletDist = 0
+    if len(foodList) > 0:
+      nearestPelletDist = min(foodList)
+
+    #the part of effect of ghost distance and it is positive related to score
+    ghostList = []
     for ghost in ghosts:
-        ghost_list.append(manhattanDistance(pacman_pos, ghost.getPosition()))
-    nearest_ghost_dist = min(ghost_list)  
-    foodstuff = sum(food_list)
-    life_buff = 0
-    if nearest_ghost_dist <=1:
-        life_buff = -100000
-    scared_times = [ghostState.scaredTimer for ghostState in ghosts]  
-    temp_1 = 0
-    temp_2 = 0
-    for time in scared_times:
-        temp_2 = time + temp_2
-    if temp_2 != 0:
-        temp_1 = -1 * life_buff 
-    score = score * 2 if nearest_pellet_dist < nearest_ghost_dist else score
-    return score - nearest_pellet_dist + nearest_ghost_dist - 0.3 * foodstuff + life_buff + 50 * temp_2 + temp_1
+      ghostPos = ghost.getPosition()
+      ghostList.append(manhattanDistance(pacman_pos,ghostPos))
+    nearestGhostDist = min(ghostList)
+
+    # the influence of a field of food
+    foodstuff = sum(foodList)
+
+    # prevent pacman from die
+    lifebuff = 0
+    if nearestGhostDist <=1:
+      lifebuff = -100000
+
+    # let pacman prefer big food to make it do not fear
+    ScaredTimes = [ghostState.scaredTimer for ghostState in ghosts]
+    timebuff = 0
+    timescore = 0
+    for time in ScaredTimes:
+      timescore = time + timescore
+    if timescore !=0:
+      timebuff = -1 * lifebuff
+
+    score = score * 2 if nearestPelletDist < nearestGhostDist else score
+    score = score - nearestPelletDist + nearestGhostDist -0.3*foodstuff + lifebuff + 50*timescore + timebuff
+    #score = score * 2 if nearestPelletDist < nearestGhostDist else score
+
+    return score
 
 # Abbreviation
 better = betterEvaluationFunction
-
